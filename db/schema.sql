5df0f00471ecf30db079e05f80ab06f835b35962
Sophoan Meas <sophoan.meas@hotmail.com>
2022-05-12 00:52:35 -0400

    add schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,30 @@ USE employees;
 
 CREATE TABLE department(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) UNIQUE NOT NULL,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
+
+CREATE TABLE role(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    INDEX role_ind (role_id),
+    INDEX manager_ind (manager_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+);
+
+
+
 
