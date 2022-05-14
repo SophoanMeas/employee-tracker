64c2519c8ccdd81419d3dc2a2d90fa916f3947ac
Sophoan Meas <sophoan.meas@hotmail.com>
2022-05-14 02:14:23 -0400

    add view  all employees

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const connection = await mysql.createConnection({
   host: process.env.HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  charset: 'utf8mb4'
 });
 
 module.exports = connection;
--- modules/selection.js
+++ modules/selection.js
@@ -18,42 +18,58 @@ FROM employee
 
     conn.execute(sql, (err, rows) => {
       if (err) throw err;
-      console.log('')
-        this.printTable(rows)
+      console.log("");
+      this.printTable(rows);
     });
   }
 
-  printTable(rows){
-      
-      const p = new Table({
-          title: 'List of Employees',
-          columns:[
-              { name: 'id', title:'ID', alignment: 'left'},
-              { name: 'first_name', title: 'First Name', alignment: 'left', color: 'green'},
-              { name: 'last_name', title: 'Last Name', alignment: 'left', color: 'green'},
-              { name: 'title', title: 'Title', alignment: 'center', color: 'yellow'},
-              { name: 'department', title: 'Department', alignment: 'center', color: 'orange'},
-              { name: 'salary', title: 'Salary', alignment: 'left'},
-              { name: 'manager', title: 'Manager', alignment: 'center', color:'cyan'},
-          ]
+  printTable(rows) {
+    const p = new Table({
+      title: "List of Employees",
+      columns: [
+        { name: "id", title: "ID", alignment: "left" },
+        {
+          name: "first_name",
+          title: "First Name",
+          alignment: "left",
+          color: "green",
+        },
+        {
+          name: "last_name",
+          title: "Last Name",
+          alignment: "left",
+          color: "green",
+        },
+        { name: "title", title: "Title", alignment: "center", color: "yellow" },
+        {
+          name: "department",
+          title: "Department",
+          alignment: "center",
+          color: "orange",
+        },
+        { name: "salary", title: "Salary", alignment: "left" },
+        {
+          name: "manager",
+          title: "Manager",
+          alignment: "center",
+          color: "cyan",
+        },
+      ],
+    });
+
+    for (let i = 0; i < rows.length; i++) {
+      p.addRow({
+        id: rows[i].id,
+        first_name: rows[i].first_name,
+        last_name: rows[i].last_name,
+        title: rows[i].title,
+        department: rows[i].dep,
+        salary: `$${rows[i].salary}`,
+        manager: rows[i].mgr,
       });
+    }
 
-
-      for (let i = 0; i < rows.length; i++){
-        p.addRow({id: rows[i].id, 
-            first_name: rows[i].first_name, 
-            last_name: rows[i].last_name,
-            title: rows[i].title,
-            department: rows[i].dep,
-            salary: `$${rows[i].salary}`,
-            manager: rows[i].mgr
-        });
-      }
-
-
-     
-      p.printTable();
-    // p.printTable(columns)
+    p.printTable();
   }
 
   exist() {
