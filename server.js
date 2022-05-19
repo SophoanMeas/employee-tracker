91610ffdebc6875504ed4f34b897b484f4b5417e
Sophoan Meas <sophoan.meas@hotmail.com>
2022-05-19 03:08:53 -0400

    final

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const connection = require('./config/connection')
-const chalkAnimation = require('chalkercli');
+const connection = require("./config/connection");
+const chalkAnimation = require("chalkercli");
 
-const {init} = require('./modules/menu')
+const { init } = require("./modules/menu");
 
 const PORT = process.env.PORT || 3001;
 const db = process.env.DB_NAME;
 
-connection.connect((err) =>{
+connection.connect((err) => {
   if (err) {
-    return console.error('error: ' + err.message);
+    return console.error("error: " + err.message);
   }
 
-// const karaoke = chalkAnimation.karaoke(`Connected to database: ${db}, Port: ${PORT}, Now listening...`, 3);
-// setTimeout(() => {
-//   karaoke.stop(); // Animation stops
-  init();
-// }, 3000);
+  const karaoke = chalkAnimation.karaoke(
+    `Connected to database: ${db}, Port: ${PORT}, Now listening...`,
+    3
+  );
+  setTimeout(() => {
+    karaoke.stop(); // Animation stops
+    init();
+  }, 2800);
 });
